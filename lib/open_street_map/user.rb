f40b8a494aaa20941a7fea0479a637109be44ea4
Christoph Bünte <info@christophbuente.de>
2012-01-02 20:46:01 +0100

    Add more attributes to the user.

--- lib/open_street_map/user.rb
+++ lib/open_street_map/user.rb
@@ -7,10 +7,31 @@ class OpenStreetMap
     # Display name
     attr_reader :display_name
 
+    # A little prosa about this user
+    attr_reader :description
+
+    # When this user was created
+    attr_reader :account_created
+
+    # All languages the user can speak
+    attr_reader :languages
+
+    # Lat/Lon Coordinates of the users home.
+    attr_reader :lat, :lon
+
+    # A picture from this user
+    attr_reader :img
+
     def initialize(attrs = {})
       attrs.stringify_keys!
-      @id           = attrs['id'].to_i if attrs['id']
-      @display_name = attrs['display_name']
+      @id               = attrs['id'].to_i if attrs['id']
+      @display_name     = attrs['display_name']
+      @lat              = attrs['home']['lat'].to_f
+      @lon              = attrs['home']['lon'].to_f
+      @languages        = attrs['languages']['lang'] if attrs['languages']
+      @description      = attrs['description']
+      @account_created  = Time.parse(attrs['account_created']) rescue nil
+      @img              = attrs['img']['href'] if attrs['img']
     end
 
   end
